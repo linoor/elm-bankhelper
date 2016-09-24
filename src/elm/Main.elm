53f92d7cc999b3404ecd87963d5cc10843e1a00c
linoor <michal.pomaranski@gmail.com>
2016-09-24 12:51:19 +0200

    moved styles to scss

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,62 +2,79 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
 import Html.Events exposing ( onClick )
+import String
+import Html.Attributes exposing ( readonly )
 
--- component import example
-import Components.Hello exposing ( hello )
 
 
 -- APP
-main : Program Never
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
-type alias Model = Int
+type alias Model = { covered : List Int }
 
-model : number
-model = 0
+pass : String
+pass = "a very long password"
+
+model : Model
+model = { covered = [] }
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg = Swap Int |
+           Unselect |
+           Select
 
 update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
-
+update msg model = case msg of
+  Swap fieldId  -> if List.member fieldId model.covered
+    then { model | covered = List.filter (\x -> x /= fieldId) model.covered }
+    else { model | covered = fieldId :: model.covered }
+  Unselect -> { model | covered = [] }
+  Select   -> { model | covered = [1..(String.length pass)] }
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
+  let
+      contains : Int -> Bool
+      contains fieldId = List.member fieldId model.covered
+
+      letter fieldId  = li
+                    [ class "fields" ]
+                    [ input
+                        [class "field",
+                         value (getNthPassLetter fieldId),
+                         readonly True,
+                         onClick (Swap fieldId)
+                         ]
+                        []
+                    , div
+                        [ style [("text-align", "center")] ]
+                        [ text (toString fieldId) ]
+                    ]
+
+      getNthPassLetter : Int -> String
+      getNthPassLetter n = pass |>
+                           String.toList |>
+                           List.drop (n-1) |>
+                           List.head |>
+                           Maybe.withDefault '?' |>
+                           String.fromChar
+
+      isDisabled : Int -> Bool
+      isDisabled fieldId = List.member fieldId model.covered
+
+  in div
+        []
+        [ List.map letter [1..(String.length pass)] |> ul [class "fields"]
+        , div
+          []
+          [ button [ onClick Unselect, class "unselect" ] [ text "unselect all" ]
+          , button [ onClick Select, class "select" ] [ text "select all" ]
           ]
         ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
